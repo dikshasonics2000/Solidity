1239e3d9ef15c9f5e2eab5d24a8c78216a21768b
Remix Workspace <>
2022-08-05 22:39:00 +0530

    first commit

---
+++ variables.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract variables{
+    bool a = false;
+    uint8 b = 8;
+    uint c = 234233;
+    uint256 d = 3344454553232343454443434334;
+    // ufixed8x1 e  = 3.6;
+    //fixed f = 34565.8;
+    address g = 0x5c6B0f7Bf3E7ce046039Bd8FABdfD3f9F5021678;
+    
+
+
+}
